a209893046ab1e961c3c89793c3eaa1800a18af5
pizzuhh <admin@pizzuhh.dev>
2024-06-28 17:34:00 +0300

    Epic IDT fail

---
+++ src/stage2/include/idt.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "libc/stdint.h"
+#include "libc/stdio.h"
+
+typedef struct {
+    uint16_t    offset_low;
+    uint16_t    segment_selector;
+    uint8_t     reserved; // must be 0
+    uint8_t     flags;
+    uint16_t    offset_high;
+} __attribute__((packed)) idt_entry;
+
+typedef struct {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed)) idtr_t;
+
+idt_entry idt[256];
+idtr_t idtr;
+
+typedef struct {
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+    uint32_t sp;
+    uint32_t ss;
+} __attribute__((packed)) interrupt_frame;
+
+
+enum {
+    TRAP_GATE = 0x8E,
+    INTERRUPT_GATE = 0x8F
+} IDT_GATE_TYPE;
+
+__attribute__((interrupt)) void default_exception_handler(interrupt_frame *frame) {
+    volatile char *video_memory = (char*)0xB8000;
+    video_memory[0] = 'X'; // do something to verify it's working
+    halt;
+}
+
+__attribute__((interrupt)) void default_exception_handler_err_code(interrupt_frame *frame, uint32_t err_code) {
+    halt;
+}
+
+__attribute__((interrupt)) void default_interrupt_handler(interrupt_frame *frame) {
+    halt;
+}
+
+void set_idt_descriptor(uint8_t entry, void *isr, uint8_t flags) {
+    idt_entry *idt_ent = &idt[entry];
+    idt_ent->offset_low = (uint32_t)isr & 0xFFFF;
+    idt_ent->segment_selector = 0x08;
+    idt_ent->reserved = 0;
+    idt_ent->flags = flags;
+    idt_ent->offset_high = ((uint32_t)isr >> 16) & 0xFFFF;
+}
+
+void init_idt(void) {
+    idtr.limit = (uint16_t)(sizeof(idt_entry)*256)-1;
+    idtr.base = (uint32_t)&idt[0];
+    for (uint8_t entry; entry < 32; ++entry) {
+        if (entry == 8 || entry == 10 || entry == 11 || entry == 12 
+        || entry == 13 || entry == 14 || entry == 17 || entry == 21) {
+            set_idt_descriptor(entry, default_exception_handler_err_code, TRAP_GATE);
+        } else {
+            set_idt_descriptor(entry, default_exception_handler, TRAP_GATE);
+        }
+    }
+
+    for (uint8_t entry = 32; entry < 256; entry++) {
+        set_idt_descriptor(entry, default_interrupt_handler, INTERRUPT_GATE);
+        
+    }
+
+    asm volatile ("lidt %0" : : "memory"(idtr));
+    asm volatile ("sti");
+}
